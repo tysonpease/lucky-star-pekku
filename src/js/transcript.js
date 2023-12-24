472bfe0cd655ba7cd98ade4052cfaed01ab38524
Steve Fulghum <steve@fulghum.digital>
2023-12-24 14:48:40 -0500

    format

--- src/js/transcript.js
+++ src/js/transcript.js
@@ -4,15 +4,15 @@ export function init() {
     let transcriptList = document.getElementById("active-transcript");
     let transcripts = transcriptList.children;
     transcripts[0].style.display = "block";
-    
+
     if (!languageList) {
         // If the language list was not built into this page (i.e. there was only one transcript to display), 
         // then don't set up event listeners for the language list
         return;
     }
-    
+
     languageList.selectedIndex = 0;
-    languageList.addEventListener("click", function() {
+    languageList.addEventListener("click", function () {
         for (let i = 0; i < transcripts.length; i++) {
             transcripts[i].style.display = i === languageList.selectedIndex ? "block" : null;
         }
