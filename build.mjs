2e4e8450fd3fae422e8775eedd8f3dfc19a25e8d
Steve Fulghum <steve@fulghum.digital>
2023-12-25 16:00:49 -0500

    update workflow

--- build.mjs
+++ build.mjs
@@ -1,9 +1,10 @@
 import { exec } from "child_process";
 
-exec(`${process.env.PYTHON} build_site.py`, { cwd: `${process.cwd()}/src/scripts`}, (error, stdout, stderr) => {
-    if (error) {
-        return console.error(error)
-    }
+const buildCommand = `${process.env.PYTHON} build_site.py --delete-scheduled-posts`
+const options = { cwd: `${process.cwd()}/src/scripts` }
+const handleOutput = (error, stdout, stderr) => {
+    if (error) { return console.error(error) }
+    return console.log(stdout, stderr)
+}
 
-    console.log(stdout, stderr)
-})
+exec(buildCommand, options, handleOutput)
